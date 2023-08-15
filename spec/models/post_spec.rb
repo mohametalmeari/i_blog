3965c4b9363d4ce087d37f3dba586a3038b26fac
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-15 11:03:57 -0700

    Test update_posts_counter

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do 
-  
+RSpec.describe Post, type: :model do
   it 'title should be present' do
     user_tom = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     post = Post.new(author: user_tom, title: nil, text: 'JS Details')
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
   it 'name should be present' do
     user = User.new(name: nil, photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     expect(user).to_not be_valid
   end
-  
+
   it 'PostsCounter must be an integer greater than or equal to zero' do
     user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     user.posts_counter = -1
     expect(user).to_not be_valid
+  end
+
+  it 'updates posts counter' do
+    user_jack = User.create(name: 'Jack', photo: 'https://', bio: 'Teacher from USA.')
+    first_post = Post.create(author: user_jack, title: 'JS', text: 'JS Details')
+    second_post = Post.create(author: user_jack, title: 'React', text: 'React Details')
+    user_jack.update_posts_counter
+    expect(user_jack.posts_counter).to be 2
   end
 end
