35b3b9ac3664f99d32dc2d4aa02e5a10253d56e6
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-15 05:22:39 -0700

    Create model classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Classes with methods
+
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id'
+
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  def update_posts_counter
+    self.update(posts_counter: self.posts.count)
+  end
+end
+
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_likes_counter
+    self.update(likes_counter: self.likes.count)
+  end
+
+  def update_comments_counter
+    self.update(comments_counter: self.comments.count)
+  end
+end
+
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+end
+
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+end
