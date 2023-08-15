b88ab286b09fb61e549e70b75edf178537cddccc
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-15 05:23:23 -0700

    Add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,24 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 end
+
+# Data
+
+user_tom = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+user_lilly = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+user_lexa = User.create(name: 'Lexa', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from USA.')
+
+first_post = Post.create(author: user_tom, title: 'JS', text: 'JS Details')
+second_post = Post.create(author: user_tom, title: 'React', text: 'React Details')
+third_post = Post.create(author: user_lilly, title: 'Ruby', text: 'Ruby Details')
+forth_post = Post.create(author: user_lexa, title: 'RoR', text: 'RoR Details')
+
+Like.create(post: first_post, author: user_lilly)
+Like.create(post: third_post, author: user_lexa)
+
+Comment.create(post: first_post, author: user_tom, text: 'Dynamic scripting language' )
+Comment.create(post: first_post, author: user_tom, text: 'Client-side web development' )
+Comment.create(post: first_post, author: user_tom, text: 'Manipulates HTML/CSS content' )
+Comment.create(post: first_post, author: user_lilly, text: 'Event-driven programming model' )
+Comment.create(post: first_post, author: user_lilly, text: 'Supports asynchronous operations' )
+Comment.create(post: first_post, author: user_lexa, text: 'Extensive third-party libraries' )
