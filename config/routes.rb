3195b8711e24abf3c982514f15e8bb8bfa3ed2b2
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-21 01:09:13 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/posts', to: 'posts#index', as: 'posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'post'
-  get '/users', to: 'usres#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
+  get 'users/:user_id/posts', to: 'posts#index', as: 'posts'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post'
+  get 'users', to: 'users#index', as: 'users'
+  get 'users/:id', to: 'users#show', as: 'user'
 end
