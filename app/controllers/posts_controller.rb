14ac2e214bad28449beda064298bbac52780adac
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-21 07:17:24 -0700

    Complete list of posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   layout 'standard'
   def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author: @user)
     render status: 200
   end
 
