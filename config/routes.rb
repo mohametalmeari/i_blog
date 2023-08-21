e435c7ab29717ec47847d5eda67d6723036366cf
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-21 01:49:15 -0700

    Use resources to group and nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/:user_id/posts', to: 'posts#index', as: 'posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post'
-  get 'users', to: 'users#index', as: 'users'
-  get 'users/:id', to: 'users#show', as: 'user'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
