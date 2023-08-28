01f5fae2e2e2e840ffb0ca41618f3d9a2b04f73e
TamaraDimov <tamemojsoska@gmail.com>
2023-08-28 11:39:21 +0200

    user show page tests

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -25,4 +25,38 @@ class UsersTest < ApplicationSystemTestCase
         click_on "Tom"
         assert_text "User's Page"
     end
+       test 'user profile image' do 
+        visit 'users/1'
+        assert_selector 'img[alt="User Photo"]'
+    end
+    test 'user name' do 
+        visit 'users/1'
+        assert_text "Tom"
+    end
+    test 'user number of posts' do 
+        visit 'users/1'
+        assert_text "Number of posts: 2", count: 1
+    end
+    test 'user bio' do 
+        visit 'users/1'
+        assert_text "Bio"
+    end
+    test 'user first 3 posts' do 
+        visit 'users/1'
+        assert_text "xx", count: 3
+    end
+    test 'view all posts button' do 
+        visit 'users/1'
+        assert_text "See all posts"
+    end
+    test 'user post link' do 
+        visit 'users/1'
+        click_on "See all posts"
+        assert_text "Post"
+    end
+    test 'user post link' do 
+        visit 'users/1'
+        click_on "See all posts "
+        assert_text "List of posts"
+    end
 end
