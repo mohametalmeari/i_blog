db4201a7896ca2069a23bdf167e93924eae8b875
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-30 16:24:16 +0300

    Add authorizatoin for delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
+
+  load_and_authorize_resource
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, notice: 'Access denied' 
+  end
+
+  
     def destroy
       @comment = Comment.find(params[:id])
         @comment.destroy
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
     can :create, :all
     # can :create, Like # everyone can create posts, comments and likes
     can :destroy, Post, author_id: user.id # everyone can create posts, comments and likes
+    can :destroy, Comment, author_id: user.id # everyone can create posts, comments and likes
 
     # can :manage, Post, user_id: user.id # user with id = (user_id of Friend) can manage Friend
     # can :manage, :all if user.role == "admin" # user with admin = true can manage all database
