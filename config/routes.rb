3a55e333246fbc466b8dcad5eb6b0de0f7ed8230
TamaraDimov <tamemojsoska@gmail.com>
2023-08-31 12:09:55 +0200

    install rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
   get 'users/new', to: 'posts#new', as: 'new'
   post 'users/new', to: 'posts#create', as: 'create_post'
